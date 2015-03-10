413cfb64d116eacd2f555518fc5e49d9367b5b1b
Joe Nelson <cred+github@begriffs.com>
2015-03-10 16:56:00 -0700

    Archive data in chunks

--- index.js
+++ index.js
@@ -2,50 +2,56 @@ var influx = require('influx');
 var AWS = require('aws-sdk');
 
 var glacier_client = new AWS.Glacier({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.REGION
-    });
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.REGION
+});
 
-var influx_opts = {
-      host : 'localhost',
-      port : 8086,
-      username : 'root',
-      password : 'root',
-      database : 'metrics'
-    };
+/* Need root to delete rows */
+var influx_client = influx({
+  host : 'localhost',
+  port : 8086,
+  username : 'root',
+  password : 'root',
+  database : 'metrics'
+});
 
-var influx_client = influx(influx_opts);
+var ROW_BATCH = 5000;
+var ALL_BEFORE = (new Date()).getTime() - (60*60*24*7 * 1000);
+var VAULT_NAME = ALL_BEFORE.toString(36);
 
-function archive(vault_name) {
-  influx_client.query('select * from /collectd.*/;', function(err, rows) {
+glacier_client.createVault({vaultName: VAULT_NAME}, function(err) {
+  if(err) { throw err; }
+  getRows();
+});
+
+///////////////////////////////////////////////////////////////////
+
+function getRows() {
+  var get_query = 'select * from /collectd.*/ where time < ' + ALL_BEFORE + 'ms limit ' + ROW_BATCH + ';';
+
+  influx_client.query(get_query, function(err, rows) {
     if(err) { throw err; }
-
-    if(rows && rows[0] && rows[0].points && rows[0].points[0] && rows[0].points[0][0]) {
-      glacier_client.uploadArchive({vaultName: vault_name, body: JSON.stringify(rows)}, function(err, data) {
-        if(err) { throw err; }
-        var last_time = rows[0].points[0][0] + 1;
-        var q = 'delete from /collectd.*/ where time < ' + last_time + ';';
-
-        influx_client.query(q, function(err, data) {
-          if(err) { throw err; }
-        });
-      });
-    }
+    archive(rows);
   });
 }
 
-glacier_client.listVaults({}, function(err, data)  {
-  var vault_name = (new Date()).getTime().toString(36);
-  if(err) { throw err; }
-  if(!data.VaultList.length) {
-    glacier_client.createVault({vaultName: vault_name}, function(err) {
-      if(err) { throw err; }
-      archive(vault_name);
-    });
-  } else {
-    vault_name = data.VaultList[0].VaultName;
-    archive(vault_name);
-  }
-});
+function archive(rows) {
+  if(!rows && !rows.length) { return; }
+  var furthest_batch_past = rows[0].points[rows[0].points.length-1][0];
 
+  glacier_client.uploadArchive({vaultName: VAULT_NAME, body: JSON.stringify(rows)}, function(err) {
+    if(err) { throw err; }
+    deleteRows(furthest_batch_past);
+  });
+}
+
+function deleteRows(after) {
+  after -= 1;
+
+  var delete_query = 'delete from /collectd.*/ where time < ' + ALL_BEFORE + 'ms and time > ' + after + 'ms;';
+  influx_client.query(delete_query, function(err) {
+    if(err) { throw err; }
+    getRows();
+  });
+}
